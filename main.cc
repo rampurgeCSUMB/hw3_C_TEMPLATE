3d3253bbaa1b76978bb9426124bba6bc5ea8b63c
rampurgeCSUMB <98712477+rampurgeCSUMB@users.noreply.github.com>
2024-02-17 07:35:16 -0800

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,24 @@
+/*
+ * INSTRUCTION:
+ *     This is a C++ starting code for hw 3.
+ */
+
+// Finish the head comment with Title, Abstract, Name, and Date.
+/*
+ * Title: .cc
+ * Abstract: Write the main purpose of the program.
+ * Name: Write your name
+ * Date: MM/DD/YYYY
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Your code should be here.
+    // The following is a just sample statement.
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
